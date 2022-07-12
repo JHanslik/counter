898a8750c290a2e40af866de2bb15e36f1d5254f
JHanslik <julien.hanslik@gmail.com>
2022-07-12 17:04:52 +0200

    exercice done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import React from "react";
+import Counter from "./components/Counter";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      count1: 0,
+      count2: 0
+    }
+  }
+
+  handleClickMinus1 = (e) => {
+    const countValue1 = this.state.count1
+    const countValue2 = this.state.count2
+    if (this.state.count1 > 0) {
+      this.setState({
+      count1: countValue1 - 1  
+    })
+    }
+  }
+  handleClickPlus1 = (e) => {
+    const countValue1 = this.state.count1
+    const countValue2 = this.state.count2
+    if (this.state.count1 < 100 && countValue1 < countValue2) {
+      this.setState({
+        count1: countValue1 + 1
+      })  
+    }
+    else if (this.state.count1 < 100){
+      this.setState({
+        count1: countValue1 + 1,
+        count2: countValue2 + 1
+      })
+    }
+    
+  }
+
+  handleClickMinus2 = (e) => {
+    const countValue1 = this.state.count1
+    const countValue2 = this.state.count2
+    if (this.state.count2 > 0 && countValue1 < countValue2) {
+      this.setState({
+      count2: countValue2 - 1  
+      })
+    }
+    else if (this.state.count2 > 0){
+      this.setState({
+        count1: countValue1 - 1,
+        count2: countValue2 - 1 
+      })
+    }
+
+  }
+  handleClickPlus2 = (e) => {
+    const countValue1 = this.state.count1
+    const countValue2 = this.state.count2
+    this.setState({
+      count2: countValue2 + 1
+    })
+  }
+  render() {
+    return (
+      <div className="container bg-light rounded p-4 text-center">
+        <h1>Counter</h1>
+        <div className="d-flex">
+          <Counter count={this.state.count1} increment={this.handleClickPlus1} substract={this.handleClickMinus1} title="Counter 1"/>
+          <Counter count={this.state.count2} increment={this.handleClickPlus2} substract={this.handleClickMinus2} title="Counter 2"/>  
+        </div>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Counter.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class Counter extends React.Component {
+    render(){
+        return (
+                <div className="border m-2">
+                    <h2>{this.props.count}</h2>
+                    <button className="bg-danger m-2 btn-lg" onClick={this.props.substract}>
+                        <i class="bi bi-dash-circle-fill"></i>
+                    </button>
+                        <button className="bg-success m-2 btn-lg" onClick={this.props.increment}>
+                    <i class="bi bi-plus-circle-fill"></i>
+                    </button>
+                    <h3>{this.props.title}</h3>
+                </div>
+        )
+    }
+}
+
+export default Counter
